2c6feda47bb193171e79b8d56e7f218a5fbe9d93
Jadedinhh <mbd77@drexel.edu>
2022-01-17 14:07:03 -0500

    jquery

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,20 @@
+import $ from "jquery";
+
+let $nav = $('nav[data-nav]');
+let isVisible = false;
+
+$(".nav-btns").on("click", function(){
+    // console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+})
+
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
