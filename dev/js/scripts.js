888b35a0af1a699c885ab50f2f1506087b2a2e7f
Jadedinhh <mbd77@drexel.edu>
2022-01-17 13:59:56 -0500

    scrolltrigger

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,17 +1,4 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import $ from "jquery";
-
-let $nav = $('nav[data-nav]');
-let isVisible = false;
-
-$(".nav-btns").on("click", function(){
-    // console.log("click");
-    if(isVisible === false){
-        $nav.show();
-        isVisible = true;
-    }
-    else{
-        $nav.hide();
-        isVisible = false;
-    }
-})
+gsap.registerPlugin(ScrollTrigger);
