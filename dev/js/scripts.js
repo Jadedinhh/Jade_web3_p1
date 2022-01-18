648744d77475b4f16b63a6f6998761559513190f
Jadedinhh <mbd77@drexel.edu>
2022-01-18 11:18:31 -0500

    forloops

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,9 +15,34 @@ $(".nav-btns").on("click", function(){
     }
 })
 
-import { skillsTrigger } from "./scrollAnimation.js"
-window.addEventListener('load', function(){
-   skillsTrigger("right center", ".bigquote","#quote"); 
+import { Slidefromleft, Slidefromright } from "./scrollAnimation.js"
 
-   skillsTrigger("left center",".gallery-image","#human");
-})
+
+window.addEventListener('load', function(){
+
+
+    let triggerElement = [ "#quote", "#gallery", "#quote", "#gallery"];
+
+    let animationElement = [ "#quote h1", "#h1", "#quote h2", "#h5" ];
+
+    // let buttonIndex = 0
+
+    for (let i = 0; i < triggerElement.length; i++){
+
+        if (i === triggerElement.length/2){
+            Slidefromleft (triggerElement[i], animationElement[i]);}
+            else{
+                Slidefromright(triggerElement[i], animationElement[i]);
+            }
+        }
+
+
+//    skillsTrigger("right center", ".bigquote","#quote"); 
+
+//    skillsTrigger("left center",".gallery-image","#human");
+
+
+
+
+    });
+
--- dev/js/scrollAnimation.js
+++ dev/js/scrollAnimation.js
@@ -5,21 +5,57 @@ gsap.registerPlugin(ScrollTrigger);
 
 // gsap.set(".bigquote", {transformOrigin:"right center"});
 
-
-export function skillsTrigger(originDirection, animationElement, triggerElement){
+export function Slidefromleft( triggerElement, animationElement){
     const tl = new gsap.timeline();
 
-    tl.from(animationElement,{
-        duration:5, 
-        scaleX:0, 
-        transformOrigin: originDirection,
-        scrollTrigger: {
+    tl.from(animationElement, {
+        duration: 1,
+        x: 500,
+        alpha:0,
+        scrollTrigger:{
             trigger: triggerElement,
-            start:"top 5%",
-            end: "bottom 60%",
-            toggleActions: "restart none resume none",
-            markers: true,
-            scrub: 1
+            scrub: true,
+            tonggleActions: "restart none resume none",
+            markers: true
         }
     });
 }
+
+export function Slidefromright( triggerElement, animationElement){
+    const tl = new gsap.timeline();
+
+    tl.from(animationElement, {
+        duration: 5,
+        x: -500,
+        alpha:0,
+        scrollTrigger:{
+            trigger: triggerElement,
+            scrub: true,
+            tonggleActions: "restart none resume none",
+            markers: true
+        }
+    });
+}
+
+
+
+
+
+
+// export function skillsTrigger(originDirection, animationElement, triggerElement){
+//     const tl = new gsap.timeline();
+
+//     tl.from(animationElement,{
+//         duration:5, 
+//         scaleX:0, 
+//         transformOrigin: originDirection,
+//         scrollTrigger: {
+//             trigger: triggerElement,
+//             start:"top 5%",
+//             end: "bottom 60%",
+//             toggleActions: "restart none resume none",
+//             markers: true,
+//             scrub: 1
+//         }
+//     });
+// }
