abb8127c8824b0957f224915c15791c5aa72fb47
Jadedinhh <mbd77@drexel.edu>
2022-01-18 01:13:59 -0500

    minorchanges

--- dev/js/scrollAnimation.js
+++ dev/js/scrollAnimation.js
@@ -10,16 +10,16 @@ export function skillsTrigger(originDirection, animationElement, triggerElement)
     const tl = new gsap.timeline();
 
     tl.from(animationElement,{
-        // duration:5, 
+        duration:5, 
         scaleX:0, 
         transformOrigin: originDirection,
         scrollTrigger: {
             trigger: triggerElement,
             start:"top 5%",
-            end: "bottom 70%",
+            end: "bottom 60%",
             toggleActions: "restart none resume none",
             markers: true,
-            scrub: true
+            scrub: 1
         }
     });
 }
