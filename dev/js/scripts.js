d65fd99242a8b60f3a4bb05660dea64c02b7abac
Jadedinhh <mbd77@drexel.edu>
2022-01-24 01:03:46 -0500

    script basic

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,20 +1,3 @@
-import $ from "jquery";
-
-let $nav = $('nav[data-nav]');
-let isVisible = false;
-
-$(".nav-btns").on("click", function(){
-    // console.log("click");
-    if(isVisible === false){
-        $nav.show();
-        isVisible = true;
-    }
-    else{
-        $nav.hide();
-        isVisible = false;
-    }
-})
-
 import { Slidefromleft, Slidefromright } from "./scrollAnimation.js"
 
 
