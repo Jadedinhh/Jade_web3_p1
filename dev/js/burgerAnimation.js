a5495e65164d9e32357688c3da983fec40345006
Jadedinhh <mbd77@drexel.edu>
2022-02-03 10:54:57 -0500

    update hidden menu

--- dev/js/burgerAnimation.js
+++ dev/js/burgerAnimation.js
@@ -4,20 +4,20 @@ const burgerBtn = document.querySelector("#burger-container");
 
 gsap.set(".burger-lines",{transformOrigin:"left center"});
 
-// let isMenuOpen = false;
+let isMenuOpen = false;
 let burgerAnimation = gsap.timeline({paused:true});
 
-burgerAnimation.to(".burger-lines",{duration:0.25, scaleY:2})
+burgerAnimation.to(".burger-lines",{duration:0.25, y:"+=2"})
 .addPause("backToLines")
 .addLabel("openMenu")
-.to("#top-line",{duration:0.25, rotation:-45,y:"-=2"},"cross")
+.to("#top-line",{duration:0.25, rotation:-45,y:"+=2"},"cross")
 // .to("#middle-line",{duration:0.25, scale:0, transformOrigin:"center"},"cross")
-.to("#bottom-line",{duration:0.25, rotation:45,y:"+=2"},"cross")
+.to("#bottom-line",{duration:0.25, rotation:45,y:"-=2"},"cross")
 .addPause()
 .addLabel("closeMenu")
-.to("#top-line",{duration:0.25, rotation:0,y:"+=2"},"uncross")
+.to("#top-line",{duration:0.25, rotation:0,y:"-=2"},"uncross")
 // .to("#middle-line",{duration:0.25, scale:1, transformOrigin:"center"},"uncross")
-.to("#bottom-line",{duration:0.25, rotation:0,y:"-=2"},"uncross")
+.to("#bottom-line",{duration:0.25, rotation:0,y:"+=2"},"uncross")
 
 export function burgerActions(){
     burgerBtn.addEventListener("mouseenter",() =>{
@@ -35,6 +35,27 @@ export function burgerActions(){
         //check to see if the class of selected is on the burger container, and if so don't allow the mouse enter animation
         if(!burgerBtn.classList.contains("selected")){
             burgerAnimation.reverse("backToLines");
+        }
+    });
+
+    burgerBtn.addEventListener("click",(e) =>{
+        console.log("click");
+        // call the resize function
+        // resizePage(isMenuOpen);
+        
+        if(isMenuOpen === false){
+            // animate the burger into an X
+            burgerAnimation.play("openMenu");
+            // add a class to the burger container of selected
+            burgerBtn.classList.add("selected");
+            isMenuOpen = true;
+        }else{
+            e.preventDefault;
+            // animate the burger into an X
+            burgerAnimation.play("closeMenu");
+            // add a class to the burger container of selected
+            burgerBtn.classList.remove("selected");
+            isMenuOpen = false;
         }
     });
 
