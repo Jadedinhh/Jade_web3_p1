5354c713be9219723050e2d6fb832eadd448c8f4
Jadedinhh <mbd77@drexel.edu>
2022-02-03 13:27:53 -0500

    testing

--- dev/js/pageResize.js
+++ dev/js/pageResize.js
@@ -3,10 +3,10 @@ import { gsap } from "gsap";
 let main = document.querySelector("main");
 let mainTL = gsap.timeline({paused:true});
     mainTL.to(".grey",{duration:0.5, backgroundColor:"#fff"},"change")
-    .to(main,{duration:0.5, x:"+=200", transformOrigin: "right"},"change");
+    .to(main,{duration:0.5, x:"+=205", transformOrigin: "left"},"change");
 
 let hiddenMenuTL = gsap.timeline({paused:true});
-    hiddenMenuTL.from(".hiddenMenuLink",{duration:0.5, x:"+=50", alpha:0, stagger:0.25},"+=0.5");
+    hiddenMenuTL.from(".hiddenMenuLink",{duration:0.5, x:"-=40", alpha:0, stagger:0.25},"+=0.5");
 
 
 export function movePage(isOpen){
@@ -17,6 +17,7 @@ export function movePage(isOpen){
         gsap.set("#hidden",{zIndex:100});
     }else{
         mainTL.reverse();
+        // hiddenMenuTL.reverse();
         gsap.set("#hidden",{zIndex:-1});
 
     }
