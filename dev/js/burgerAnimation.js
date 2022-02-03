e64b68e02bfaa8d0db8c945802875c9a4e495d23
Jadedinhh <mbd77@drexel.edu>
2022-02-03 04:31:02 -0500

    working

--- dev/js/burgerAnimation.js
+++ dev/js/burgerAnimation.js
@@ -7,16 +7,16 @@ gsap.set(".burger-lines",{transformOrigin:"left center"});
 let isMenuOpen = false;
 let burgerAnimation = gsap.timeline({paused:true});
 
-burgerAnimation.to(".burger-lines",{duration:0.25, scaleX:2})
+burgerAnimation.to(".burger-lines",{duration:0.25, scaleY:2})
 .addPause("backToLines")
 .addLabel("openMenu")
 .to("#top-line",{duration:0.25, rotation:-45,y:"-=2"},"cross")
-.to("#middle-line",{duration:0.25, scale:0, transformOrigin:"center"},"cross")
+// .to("#middle-line",{duration:0.25, scale:0, transformOrigin:"center"},"cross")
 .to("#bottom-line",{duration:0.25, rotation:45,y:"+=2"},"cross")
 .addPause()
 .addLabel("closeMenu")
 .to("#top-line",{duration:0.25, rotation:0,y:"+=2"},"uncross")
-.to("#middle-line",{duration:0.25, scale:1, transformOrigin:"center"},"uncross")
+// .to("#middle-line",{duration:0.25, scale:1, transformOrigin:"center"},"uncross")
 .to("#bottom-line",{duration:0.25, rotation:0,y:"-=2"},"uncross")
 
 export function burgerActions(){
