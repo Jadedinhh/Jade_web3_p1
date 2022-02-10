9d621d5086fc5b3d497d63e87375bc0f3bbf4014
Jadedinhh <mbd77@drexel.edu>
2022-02-10 12:12:35 -0500

    last

--- dev/js/pageResize.js
+++ dev/js/pageResize.js
@@ -1,11 +1,13 @@
 import { gsap } from "gsap";
 
-gsap.set("#cover-up",{transformOrigin:"center left"});
+// gsap.set("#cover-up",{transformOrigin:"center left", scaleX:0, scaleY: 0.9});
 
 let main = document.querySelector("main");
 let mainTL = gsap.timeline({paused:true});
     mainTL.to(".grey",{duration:0.5, backgroundColor:"#fff"},"change")
     .to(main,{duration:0.5, x:"+=205", transformOrigin: "left"},"change");
+    // .to("#cover-up", {duration: 0.5, scaleY:1, ease: "none"});
+    
 
 let hiddenMenuTL = gsap.timeline({paused:true});
     hiddenMenuTL.from(".hiddenMenuLink",{duration:0.5, x:"-=40", alpha:0, stagger:0.25},"+=0.5");
@@ -22,7 +24,7 @@ export function movePage(isOpen){
     }else{
         mainTL.reverse();
         hiddenMenuTL.reverse();
-        gsap.set("#hidden",{zIndex:-100});
+        gsap.set("#hidden",{zIndex:-1});
         isOpen = false;
 
     }
